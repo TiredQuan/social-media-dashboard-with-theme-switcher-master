082e8e2d7debb8c100e793958436a10c8f1fec3f
TiredQuan <85377764+TiredQuan@users.noreply.github.com>
2021-11-08 15:24:51 +0700

    YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- apps/script.js
+++ apps/script.js
@@ -35,14 +35,28 @@
 //     setAppliedMode(getAppliedMode(newUserPref));
 // }
 let themeToggler = document.getElementById('theme-toggle')
+const toggle = document.getElementById('toggle');
+const body = document.body;
 
-themeToggler.onclick=()=>{
-    if(document.body.classList=='light'){
-        document.body.classList.remove('light')
-        document.body.classList.add('dark')
-    } else if(document.body.classList=='dark'){
-        document.body.classList.remove('dark')
-        document.body.classList.add('light')
-    }
+themeToggler.onclick= () => {
+ if(document.body.classList=='dark'){
+    toggle.checked = true;
+    document.body.classList.remove('dark')
+    document.body.classList.add('light')
 }
+else if(document.body.classList=='light'){
+    toggle.checked = false;
+    document.body.classList.remove('light')
+    document.body.classList.add('dark')
+}}
+toggle.addEventListener('input', (e) => {
+    const isChecked = e.target.checked;
+	if(isChecked) {
+        body.classList.remove('dark');
+		body.classList.add('light');
+    } else {
+        body.classList.remove('light');
+		body.classList.add('dark');
+	}
+});
 //Credit to Ayc0 for the dark theme articles in dev.to 

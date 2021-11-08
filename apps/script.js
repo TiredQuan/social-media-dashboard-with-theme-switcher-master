330fb546a20d7593d5b3e0dcaa9dd1c9712518a3
TiredQuan <85377764+TiredQuan@users.noreply.github.com>
2021-11-08 16:07:04 +0700

    FINISHED IT

--- apps/script.js
+++ apps/script.js
@@ -1,42 +1,19 @@
-// function getUserPreference(){
-//     return localStorage.getItem('theme')||'system';
-// }
-// function saveUserPreference(userPreference){
-//     localStorage.setItem('theme',userPreference);
-// }
-// function getAppliedMode(userPreference){
-//     if(userPreference==='light'){
-//         return 'light';
-//     }
-//     if(userPreference==='dark'){
-//         return 'dark';
-//     }
-
-//     //system
-//     if(matchMedia('(prefers-color-scheme: light)').matches){
-//         return 'light';
-//     }
-//     return 'dark';
-// }
-// const colorScheme = document.querySelector('meta[name="color-scheme]');
-// function setAppliedMode(mode){
-//     document.body.className=mode;
-//     colorScheme.content=mode;
-// }
-
-// const themeToggler = document.getElementById('theme-toggle');
-// let userPreference = getUserPreference();
-// setAppliedMode(getAppliedMode(userPreference));
-
-// themeToggler.onclick = () =>{
-//     const newUserPref = rotatePreferences(userPreference);
-//     userPreference=newUserPref;
-//     saveUserPreference(newUserPref);
-//     setAppliedMode(getAppliedMode(newUserPref));
-// }
 let themeToggler = document.getElementById('theme-toggle')
 const toggle = document.getElementById('toggle');
 const body = document.body;
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+if (prefersDarkScheme.matches) {
+    toggle.checked = false;
+    document.body.classList.remove('light')
+    document.body.classList.add('dark')
+} else {
+    toggle.checked = true;
+    document.body.classList.remove('dark')
+    document.body.classList.add('light')
+}
+
+
 
 themeToggler.onclick= () => {
  if(document.body.classList=='dark'){
